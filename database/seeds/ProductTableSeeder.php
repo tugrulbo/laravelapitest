e3279d777e988cee8941b9851ced47e256ccc470
tugrulb <tugrulbo@gmail.com>
2022-10-27 15:11:16 +0300

    Basics
    Created basic controllers, models, migrations, seeders and routes

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement("TRUNCATE TABLE products"); 
+
+        factory(App\Product::class,10)->create();
+    }
+}
