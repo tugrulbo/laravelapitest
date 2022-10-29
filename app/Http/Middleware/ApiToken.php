9647068d4119dca00892778e7788814fabbee20f
tugrulb <tugrulbo@gmail.com>
2022-10-29 12:16:44 +0300

    Bearer Token

---
+++ app/Http/Middleware/ApiToken.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\User;
+
+class ApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $auth = $request->header('Authorization');
+
+        if($auth){
+            $token = str_replace('Bearer','',$auth);
+
+            if($token){
+                return response()->json([
+                    'message' => 'No Bearer Token'
+                ], 401);
+            }
+
+            $user = User::where('api_token',$token)->first();
+            
+            if(!$user){
+                return response()->json([
+                    'message' => 'Invalid Bearer Token'
+                ], 401);
+               
+            }
+
+            return $next($request);
+
+        }
+        return response()->json([
+            'message' => 'Invalid Bearer Token'
+        ], 401);
+    }
+}
